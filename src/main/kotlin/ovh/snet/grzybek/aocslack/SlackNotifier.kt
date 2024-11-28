0efc365680440dba7e5f3bbf72a759b1cf8198a3
Kelli Rockwell <kelli.rockwell@growtherapy.com>
2024-11-28 14:10:42 -0800

    Skip sending messages to Slack in debug mode

--- src/main/kotlin/ovh/snet/grzybek/aocslack/SlackNotifier.kt
+++ src/main/kotlin/ovh/snet/grzybek/aocslack/SlackNotifier.kt
@@ -9,13 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired
 class SlackNotifier(
     @Value("\${aoc.slack.webhook.url}")
     private val webhookUrl: String,
+    @Value("\${aoc.slack.debug-mode:false}")
+    private val debugMode: Boolean,
     @Autowired private val logger: Logger
 ) {
     private val slack: Slack = Slack.getInstance()
 
     fun sendSlackMessage(message: String) {
         val payload = "{\"text\":\"${message}\"}"
+        if (!debugMode) {
             logger.log("Sending message to Slack: $message", Logger.LogLevel.INFO)
             slack.send(webhookUrl, payload)
+        } else {
+            logger.log("Debug mode enabled, skipping sending message to Slack: $message", Logger.LogLevel.INFO)
+        }
     }
 }
