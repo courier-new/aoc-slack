8efa297b63c955f836701e45e404fce5bb507dd5
Kelli Rockwell <kelli.rockwell@growtherapy.com>
2025-12-04 17:21:48 -0800

    Fix test

--- src/test/kotlin/ovh/snet/grzybek/aocslack/LeaderBoardTest.kt
+++ src/test/kotlin/ovh/snet/grzybek/aocslack/LeaderBoardTest.kt
@@ -47,12 +47,18 @@ class LeaderBoardTest {
     }
 
     @Test
-    fun shouldNotReturnStarWhenUserDidntExist() {
+    fun shouldReturnStarsWhenNewMemberJoins() {
         val prevLeaderBoard = createLeaderBoard(mapOf())
         val newLeaderBoard =
             createLeaderBoardWithLevels(mapOf("1" to mapOf("1" to createLevel(), "2" to createLevel())))
         val stars = prevLeaderBoard.findNewStars(newLeaderBoard, logger)
-        assertThat(stars).hasSize(0)
+        assertThat(stars).hasSize(2)
+        assertThat(stars[0].member).isEqualTo("Grzybek")
+        assertThat(stars[0].day).isEqualTo(1)
+        assertThat(stars[0].star).isEqualTo(1)
+        assertThat(stars[1].member).isEqualTo("Grzybek")
+        assertThat(stars[1].day).isEqualTo(1)
+        assertThat(stars[1].star).isEqualTo(2)
     }
 
     fun createLeaderBoardWithLevels(completionDayLevel: Map<String, Map<String, LeaderBoard.Member.Level>>): LeaderBoard {
